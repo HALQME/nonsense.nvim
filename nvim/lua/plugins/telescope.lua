fbaee32f6c9e58d5e4d93a19c1cc93793bdeab42
HAL <68320771+HALQME@users.noreply.github.com>
2024-10-22 10:05:03 +0900

    add telescope

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        event = "VeryLazy",
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        event = "VeryLazy",
+    }
+}
