a646cee452c6387e34d9cc05e04830cf18aa06f2
HAL <68320771+HALQME@users.noreply.github.com>
2024-12-02 16:50:19 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -4,8 +4,16 @@ if [ -d ${HOME}/.config/nvim ]; then
   # rename and backup
   mv ${HOME}/.config/nvim ${HOME}/.config/nvim.bak
 else
-  echo "${HOME}/.config/nvim ディレクトリが存在しません。"
+  echo "${HOME}/.config/nvim ディレクトリが存在しません。ディレクトリを新規作成します。"
 fi
 
 # make symbolic link
   ln -s "$repo_dir/nvim" ${HOME}/.config/nvim
+
+if [ $? == 0]; then
+  echo "Complete!"
+else
+  echo "Fail"
+fi
+	
+	
